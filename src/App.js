52bea10cab8feb0d0cbcd1e5a5184942f2f479ae
Amol shelke <shelkeamol897@gmail.com>
2022-11-24 14:01:19 +0530

    Adding stateful random background colors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
+const randomColors = [
+  "#FF6633",
+  "#FFB399",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+];
+
 function App() {
+  const [color, setRandomColor] = useState(randomColors[0]);
+
+  const randomColorChangeHandler = () => {
+    const randomColor = Math.ceil(Math.random() * randomColors.length);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Change Random Background color</p>
+      <button>Change</button>
     </div>
   );
 }
